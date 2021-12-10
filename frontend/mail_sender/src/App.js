66e86d4aebcf98664bd8abea770a65137c259d44
kaphley <kaphley@kali.kaphley>
2021-12-10 22:00:39 +0530

    frontend needed

--- frontend/mail_sender/src/App.js
+++ frontend/mail_sender/src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import {Form} from 'react-bootstrap';
+import "./App.css";
+import { Form } from "react-bootstrap";
 
 function App() {
   return (
@@ -10,13 +10,23 @@ function App() {
 }
 
 function MailBox() {
-  
   return (
-    <div>
-      <Form>
-        <Form.Label>Your Email</Form.Label>
-        <Form.Control type= "email" placeholder="Your email"/>
-        <Form.Text className = "text-muted">We'll never share your email with anyone</Form.Text>
+    <div className="flexParent">
+      <h2>Mail sender</h2>
+
+      <Form className="mailbox">
+        <Form.Group>
+          <Form.Label>Destination Email</Form.Label>
+          <Form.Control type="email" placeholder="Your email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Message</Form.Label>
+          <Form.Control as="texarea" rows={40} placeholder="message" />
+        </Form.Group>
       </Form>
     </div>
   );
